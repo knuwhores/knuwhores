8dd719847e548f5300a2f1c78215f075653e5922
FalconUA <mr.havu@yahoo.com>
2015-01-11 07:22:08 +0200

    initial commit!

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
+<?php
+	include('mysql.php');
+	
+?>
+
+<!DOCTYPE html>
 <html>
-	<?php echo 'Hello, world!'; ?>
-<html>
+	<head>
+		<meta content="text/html"; charset=cp1251"/>
+		<title>Шлюхи КНУ</title>
+
+		<style type="text/css">
+			body, html {
+				font-family: Arial, Helvetica, sans-serif;
+				width: 100%;
+				margin: 0;
+				padding: 0;
+				text-align: center;
+			}
+			h1 {
+				background-color: #660000;
+				color: #FFFFFF;
+				padding: 20px 0;
+				margin: 0;
+			}			
+		</style>
+
+	</head>
+	<body>
+		<h1>Шлюхи КНУ</h1>
+		<h2>Она шлюха? (да/нет)</h2>
+	</body>
+</html>
---
+++ mysql.php
@@ -0,0 +1,11 @@
+<?php
+
+	// MySQL settings
+	$user		= "admin4XLsf51";
+	$password	= "ZprjXm4nBI8D";
+	$database	= "knu";
+	$host		= "127.8.19.130";
+
+	mysql_connect($host, $user, $password);
+	mysql_select_db($database) or die("Unable to select database");
+?>
